4e7f816673ca88798233a79970abf38f34e9e2e3
Todd0554 <toddy9229@outlook.com>
2025-03-31 17:23:57 +1100

    done - optimise the main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from search import Search 
 
 # initialize the dictionary for each different data
@@ -7,56 +8,64 @@ edges = {}
 origin = {}
 destinations = {}
 
-
 # read the file path and algorithm method from command line argument
-path = sys.argv[1]
-# algorithMethod = sys.argv[2]
 
-with open(path, "r") as file:
-    section = None
-    # read line by line
-    for line in file:
-        line = line.strip()
-        if not line:
-            continue
+if len(sys.argv) == 3 :
+    path = sys.argv[1]
+    algorithMethod = sys.argv[2]
+    if os.path.isfile(path):
+        with open(path, "r") as file:
+            section = None
+        # read line by line
+        for line in file:
+            line = line.strip()
+            if not line:
+                continue
 
-        # check what session the data below belongs to
-        if line.startswith("Nodes:"):
-            section = "nodes"
-            continue
-        elif line.startswith("Edges:"):
-            section = "edges"
-            continue
-        elif line.startswith("Origin:"):
-            section = "origin"
-            continue
-        elif line.startswith("Destinations:"):
-            section = "destinations"
-            continue
+            # check what session the data below belongs to
+            if line.startswith("Nodes:"):
+                section = "nodes"
+                continue
+            elif line.startswith("Edges:"):
+                section = "edges"
+                continue
+            elif line.startswith("Origin:"):
+                section = "origin"
+                continue
+            elif line.startswith("Destinations:"):
+                section = "destinations"
+                continue
 
-        # read data under each section
-        if section == "nodes":
-            node_id, coords = line.split(": ")
-            x, y = coords.strip("()").split(",")
-            nodes[int(node_id.strip())] = (int(x), int(y))
-        elif section == "edges":
-            edge_part, weight = line.split(": ")
-            from_to = edge_part.strip("()").split(",")
-            from_node = int(from_to[0])
-            to_node = int(from_to[1])
-            edges[(from_node, to_node)] = int(weight)
-        elif section == "origin":
-            origin_id = int(line.strip())
-            origin["origin"] = origin_id
-        elif section == "destinations":
-            dest_list = line.strip().split(";")
-            destinations["destinations"] = [int(x.strip()) for x in dest_list]
+            # read data under each section
+            if section == "nodes":
+                node_id, coords = line.split(": ")
+                x, y = coords.strip("()").split(",")
+                nodes[int(node_id.strip())] = (int(x), int(y))
+            elif section == "edges":
+                edge_part, weight = line.split(": ")
+                from_to = edge_part.strip("()").split(",")
+                from_node = int(from_to[0])
+                to_node = int(from_to[1])
+                edges[(from_node, to_node)] = int(weight)
+            elif section == "origin":
+                origin_id = int(line.strip())
+                origin["origin"] = origin_id
+            elif section == "destinations":
+                dest_list = line.strip().split(";")
+                destinations["destinations"] = [int(x.strip()) for x in dest_list]
+        # print("Nodes:", nodes)
+        # print("Edges:", edges)
+        # print("Origin:", origin)
+        # print("Destinations:", destinations)
 
-print("Nodes:", nodes)
-print("Edges:", edges)
-print("Origin:", origin)
-print("Destinations:", destinations)
 
+        # instantiate the Search class
+        search = Search(nodes, edges, origin, destinations)  
+        search.algoritm_selection(algorithMethod)
+    else:
+        print("File not found.")
+        sys.exit(1)
+else:
+    print("Please provide the file path and algorithm method as command line arguments.")
+    sys.exit(1)
 
-# instantiate the Search class
-search = Search(nodes, edges, origin, destinations)

4bacdeb4b95fce5a2fb16680d15e04ba1900a37e
Todd0554 <toddy9229@outlook.com>
2025-03-31 16:52:50 +1100

    done - initial structure of Search class

--- search.py
+++ search.py
@@ -8,20 +8,56 @@ class Search:
         self.origin = origin
         self.destination = destination
         
-    # def algoritm_selection(method):
-    #     match method:
-    #         case 'DFS':
-    #             self.DFS();
-    #         case 'BFS':
-    #             self.BFS();
-    #         case 'A*':
-    #             self.Astar();
-    #         case 'CUS1': 
-    #             self.CUS1();
-    #         case 'CUS2':
-    #             self.CUS2();
-    #         case _:
-    #             print("Invalid method selected.")
+    def algoritm_selection(method):
+        match method:
+            case 'DFS':
+                self.DFS()
+            case 'BFS':
+                self.BFS()
+            case 'GBFS':
+                self.GBFS()
+            case 'A*':
+                self.Astar()
+            case 'CUS1': 
+                self.CUS1()
+            case 'CUS2':
+                self.CUS2()
+            case _:
+                print("Invalid method selected.")
+                
+    def DFS(self):
+        
+        return
+        
+    def BFS(self):
+        
+        return
+        
+        
+        
+    def GBFS(self):
+        
+        return
+        
+        
+    
+    def Astar(self):
+        
+        return
+        
+        
+    def CUS1(self):
+        
+        return
+        
+        
+    def CUS2(self):
+        
+        return
+        
+    
+    
+    
        
         
 
@@ -76,10 +112,10 @@ with open(path, "r") as file:
             dest_list = line.strip().split(";")
             destinations["destinations"] = [int(x.strip()) for x in dest_list]
 
-print("Nodes:", nodes)
-print("Edges:", edges)
-print("Origin:", origin)
-print("Destinations:", destinations)
+# print("Nodes:", nodes)
+# print("Edges:", edges)
+# print("Origin:", origin)
+# print("Destinations:", destinations)
 
 
 # instantiate the Search class
